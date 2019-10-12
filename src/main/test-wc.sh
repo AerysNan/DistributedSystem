ccfc4bb0130d2d79618110dc5ee3f8cba2c85f79
AerysNan <aerysnan@gmail.com>
2019-10-12 20:35:27 +0800

    fix test

--- src/main/test-wc.sh
+++ src/main/test-wc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 go run wc.go master sequential pg-*.txt
-sort -n -k2 mrtmp.wcseq | tail -10 | diff - mr-testout.txt > diff.out
+sort -n -k2 mrtmp.wcseq | tail -10 | diff --strip-trailing-cr - mr-testout.txt > diff.out
 if [ -s diff.out ]
 then
 echo "Failed test. Output should be as in mr-testout.txt. Your output differs as follows (from diff.out):" > /dev/stderr
@@ -8,4 +8,5 @@ echo "Failed test. Output should be as in mr-testout.txt. Your output differs as
 else
   echo "Passed test" > /dev/stderr
 fi
-
+rm mrtmp.*
+rm diff.out
